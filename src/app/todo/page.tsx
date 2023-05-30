eb0fc1c4f833278234387143fc8ed3bdd0f1cf0f
dengoyoon <engus927@naver.com>
2023-05-30 23:58:02 +0900

    서버사이드 렌더링

---
+++ src/app/todo/page.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import React, { useState } from "react";
+
+type Props = {};
+
+type TodoType = {
+    userId: string;
+    id: string;
+    title: string;
+    completed: boolean;
+  };
+
+const fetchTodos = async () => {
+  const results = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const todos: TodoType[] = await results.json();
+  return todos;
+};
+
+async function TodosList({}: Props) {
+  const todos: TodoType[] = await fetchTodos();
+  //   console.log(todos);
+  return (
+    <div>
+      {todos?.map((todo) => (
+        <p key={todo.id} className="text-base">
+          <Link href={`/todos/${todo.id}`}>Todo #{todo.id}</Link>
+        </p>
+      ))}
+    </div>
+  );
+}
+
+export default TodosList;
